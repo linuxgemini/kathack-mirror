d61ec607cd86be22312e2a4ba55249e3493e92a5
Ilteris EROGLU <ilteris23.54@gmail.com>
2015-08-02 23:59:41 +0300

    helo.

---
+++ jquery.min.js
@@ -0,0 +1,15 @@
+/*!
+ * jQuery JavaScript Library v1.5.1
+ * http://jquery.com/
+ *
+ * Copyright 2011, John Resig
+ * Dual licensed under the MIT or GPL Version 2 licenses.
+ * http://jquery.org/license
+ *
+ * Includes Sizzle.js
+ * http://sizzlejs.com/
+ * Copyright 2011, The Dojo Foundation
+ * Released under the MIT, BSD, and GPL Licenses.
+ *
+ * Date: Wed Feb 23 13:55:29 2011 -0500
+ */
